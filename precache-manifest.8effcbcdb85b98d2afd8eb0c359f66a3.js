48f966351a2e400902051aadc19f08cce7e84082
Hrant-p <hrant@live.ru>
2019-12-24 17:52:23 +0400

    Updates

---
+++ precache-manifest.8effcbcdb85b98d2afd8eb0c359f66a3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91de22e111069c006eecd2277b90e7ac",
+    "url": "/beers/index.html"
+  },
+  {
+    "revision": "dd13e355f0d58f201870",
+    "url": "/beers/static/css/main.2508ad24.chunk.css"
+  },
+  {
+    "revision": "10f656d76451d3c77bea",
+    "url": "/beers/static/js/2.6383e699.chunk.js"
+  },
+  {
+    "revision": "b85c2b7a86a8fe2196cb7a9cd5db10f2",
+    "url": "/beers/static/js/2.6383e699.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dd13e355f0d58f201870",
+    "url": "/beers/static/js/main.b8dc4670.chunk.js"
+  },
+  {
+    "revision": "55a4cd3dba4e428d626f",
+    "url": "/beers/static/js/runtime-main.852eb18a.js"
+  },
+  {
+    "revision": "aa3da263c7276c195d26098303999daf",
+    "url": "/beers/static/media/blue-star.aa3da263.svg"
+  },
+  {
+    "revision": "9e8af3de487ce6e7d68ab0f25746de25",
+    "url": "/beers/static/media/yellow-star.9e8af3de.svg"
+  }
+]);
